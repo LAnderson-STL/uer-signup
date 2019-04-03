e2e79a249736b250941131cbed715ae3b18aa337
LAnderson-STL <kirsch_17@hotmail.com>
2019-04-03 10:53:55 -0500

    email val and styling not working complete

--- main.py
+++ main.py
@@ -2,27 +2,18 @@ from flask import Flask, request, redirect, render_template
 import cgi
 import os 
 
-#import jinja2
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
 
 @app.route("/home")
 def index():
     return render_template('home_page.html')
 
-
-#@app.route("/welcome", methods=['POST'])
-#def hello():
-    #first_name = request.form['first_name']
-    #return render_template('hello_greeting.html', name=first_name)
-
-#@app.route('/home')
-#def display_signup_form():
-    #return render_template('home_page.html.html')
-
-def is_empty(input):
-    if input == "":
+def is_not_empty(input):
+    if input:
         return True
     else:
         return False
@@ -39,18 +30,18 @@ def contains_space(input):
     else: 
         return False
 
-def valid_email(input):
+def not_valid_email(input):
     if "." in input and "@" in input:
-        return True
-    else:
         return False
+    else:
+        return True
 
 @app.route("/home", methods=["POST"])
 def validate():
     username = request.form["username"]
     password = request.form["password"]
     verify_password = request.form["verify_password"]
-    email = ["email"]
+    email = request.form["email"]
 
     username_error = ""
     password_error = ""
@@ -59,29 +50,25 @@ def validate():
     
 
     #username logic
-    if is_empty(username) or contains_space(username) or not_correct_length(username):
+    if contains_space(username) or not_correct_length(username):
         username_error = "Not a vaild username"
-        username = ""
+        username = username
 
     #password logic
-    if is_empty(password) or contains_space(password) or not_correct_length(password):
+    if contains_space(password) or not_correct_length(password):
         password_error = "Not a vaild password"
         password = ""
 
     #verify_password logic
     if verify_password != password:
         verify_password_error="Passwords don't match"
+        verify_password = ""
 
     #email logic:
-    if not is_empty(email) and 
-        if contains_space(email):
-            email_error = "Not a valid email"
-            email = ""
-        elif not valid_email(email):
-            email_error = "Not a valid email"
-            email = ""
-
-
+    if is_not_empty(email) and not_correct_length(email) or contains_space(email) or not_valid_email(email):
+        email_error = "Not a valid email"
+        email = email
+    
 
         
 
@@ -90,7 +77,7 @@ def validate():
     
 
 
-    if not username_error and not password_error and not verify_password_error: #and not email_error:
+    if not username_error and not password_error and not verify_password_error and not email_error:
         new_user = username
         return redirect('/welcome?new_user={0}'.format(new_user))
     else:
@@ -107,7 +94,7 @@ def validate():
 @app.route("/welcome")
 def valid_info():
     new_user = request.args.get('new_user')
-    return "<h1>Welcome, {0}!</h1>".format(new_user)
+    return render_template('welcome_page.html', name = new_user)
 
 
 
